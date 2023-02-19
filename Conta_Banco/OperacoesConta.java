1618ffe77934c085087d7a6e2923da9e0e968d10
gustavo1902 <gustavomorais1009@gmail.com>
2023-02-19 20:45:51 -0300

    19/02

--- Conta_Banco/OperacoesConta.java
+++ Conta_Banco/OperacoesConta.java
@@ -1,5 +1,7 @@
 package Conta_Banco;
 
+import java.util.Scanner;
+
 public class OperacoesConta {
 
     public void depositar(ContaBancaria conta, double valor){
@@ -21,10 +23,37 @@ public class OperacoesConta {
         conta.imprimir(); // Imprime os dados iniciais da conta
         
         OperacoesConta operacoes = new OperacoesConta();
-        operacoes.depositar(conta, 500.0);
-        conta.imprimir(); // Imprime os dados após o depósito
+        Scanner scanner = new Scanner(System.in);
         
-        operacoes.sacar(conta, -200.0);
-        conta.imprimir(); // Imprime os dados após a tentativa de saque
+        int opcao = 0;
+        
+        while (opcao != 3) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Sair");
+            opcao = scanner.nextInt();
+            
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o valor a depositar:");
+                    double valorDeposito = scanner.nextDouble();
+                    operacoes.depositar(conta, valorDeposito);
+                    conta.imprimir();
+                    break;
+                case 2:
+                    System.out.println("Digite o valor a sacar:");
+                    double valorSaque = scanner.nextDouble();
+                    operacoes.sacar(conta, valorSaque);
+                    conta.imprimir();
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
     }
 }
