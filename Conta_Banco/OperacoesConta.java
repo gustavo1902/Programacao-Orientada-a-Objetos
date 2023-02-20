9144c28994b7c5e82b660a8da16d5581cf3f7846
gustavo1902 <gustavomorais1009@gmail.com>
2023-02-19 21:01:30 -0300

    19/02

--- Conta_Banco/OperacoesConta.java
+++ Conta_Banco/OperacoesConta.java
@@ -2,58 +2,58 @@ package Conta_Banco;
 
 import java.util.Scanner;
 
+
 public class OperacoesConta {
-
-    public void depositar(ContaBancaria conta, double valor){
-        conta.setSaldo(conta.getSaldo() + valor);
-    }
-
-    public void sacar(ContaBancaria conta, double valor){
-        if (valor > 0 && valor <= conta.getSaldo()) {
-            conta.setSaldo(conta.getSaldo() - valor);
-        } else if (valor < 0) {
-            System.out.println("Valor inválido para saque!");
-        } else {
-            System.out.println("Saldo insuficiente para realizar a operação!");
-        }
-    }    
     
     public static void main(String[] args){
-        ContaBancaria conta = new ContaBancaria("João", 1234, 1000.0);
-        conta.imprimir(); // Imprime os dados iniciais da conta
         
-        OperacoesConta operacoes = new OperacoesConta();
+        // Criação das contas
+        ContaBancaria conta1 = new ContaBancaria("João", 1234, 1000.0);
+        ContaBancaria conta2 = new ContaBancaria("Maria", 5678, 500.0);
+        
+        // Exibição das informações das contas
+        System.out.println("Selecione a conta:");
+        System.out.println("1 - " + conta1.getNome() + " - Número: " + conta1.getNumero() + " - Saldo: " + conta1.getSaldo());
+        System.out.println("2 - " + conta2.getNome() + " - Número: " + conta2.getNumero() + " - Saldo: " + conta2.getSaldo());
+        
+        // Leitura da opção selecionada pelo usuário
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Opção selecionada: ");
+        int opcao = scanner.nextInt();
         
-        int opcao = 0;
-        
-        while (opcao != 3) {
-            System.out.println("Escolha uma opção:");
-            System.out.println("1 - Depositar");
-            System.out.println("2 - Sacar");
-            System.out.println("3 - Sair");
-            opcao = scanner.nextInt();
-            
-            switch (opcao) {
-                case 1:
-                    System.out.println("Digite o valor a depositar:");
-                    double valorDeposito = scanner.nextDouble();
-                    operacoes.depositar(conta, valorDeposito);
-                    conta.imprimir();
-                    break;
-                case 2:
-                    System.out.println("Digite o valor a sacar:");
-                    double valorSaque = scanner.nextDouble();
-                    operacoes.sacar(conta, valorSaque);
-                    conta.imprimir();
-                    break;
-                case 3:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-                    break;
-            }
+        // Seleção da conta de acordo com a opção selecionada pelo usuário
+        ContaBancaria contaSelecionada = null;
+        if (opcao == 1) {
+            contaSelecionada = conta1;
+        } else if (opcao == 2) {
+            contaSelecionada = conta2;
+        } else {
+            System.out.println("Opção inválida!");
+            return;
         }
+        
+        // Exibição das informações da conta selecionada
+        System.out.println("\nConta selecionada: " + contaSelecionada.getNome() + " - Número: " + contaSelecionada.getNumero() + " - Saldo: " + contaSelecionada.getSaldo());
+        
+        // Realização das operações
+        System.out.println("\nSelecione a operação:");
+        System.out.println("1 - Depositar");
+        System.out.println("2 - Sacar");
+        System.out.print("Opção selecionada: ");
+        int operacao = scanner.nextInt();
+        
+        if (operacao == 1) {
+            System.out.print("Valor a depositar: ");
+            double valor = scanner.nextDouble();
+            contaSelecionada.depositar(valor);
+            System.out.println("Novo saldo: " + contaSelecionada.getSaldo());
+        } else if (operacao == 2) {
+            System.out.print("Valor a sacar: ");
+            double valor = scanner.nextDouble();
+            contaSelecionada.sacar(valor);
+            System.out.println("Novo saldo: " + contaSelecionada.getSaldo());
+        } else {
+            System.out.println("Opção inválida!");
+        }        
     }
 }
